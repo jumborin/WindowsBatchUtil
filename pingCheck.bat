b90a8814660abdb10aae1d883404531899c27eab
Jumborin <y.f.architect@gmail.com>
2016-05-19 07:39:57 +0900

    Update pingCheck.bat

--- pingCheck.bat
+++ pingCheck.bat
@@ -1,8 +1,10 @@
+REM 結果ファイルの出力ディレクトリ
 set ROOT_DIR=C:\Users\jumborin\Desktop\
 
+REM 生存確認したい端末のIPアドレス
 set IP_ADDRESS=192.168.11.1
 
-REM 日付時刻14桁を変数に代入
+REM 日付時刻15桁(yyyymmdd_HHMMSS)を変数に代入
 set TIME=%DATE:~-10,4%%DATE:~-5,2%%DATE:~-2%_%TIME:~0,2%%TIME:~3,2%%TIME:~6,2% 
 
 REM テンプファイルに結果を書き出し
@@ -11,8 +13,9 @@ ping %IP_ADDRESS% | find "パケット数:" > temp.txt
 REM 結果を読み込み、変数に代入
 FOR /f "delims=" %%t IN (temp.txt) DO SET RESULT=%%t
 
+REM テンプファイルを削除
+del /F temp.txt
+
 REM 結果ファイルに日時と結果を書き出し
 echo %TIME%%RESULT% >> %ROOT_DIR%checkResult.txt
 
-REM テンプファイルを削除
-del /F temp.txt
