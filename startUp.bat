c39796fb5ef7e7bbc8eedfddb79c1d3d39ed9ab8
jumborin <y.f.architect@gmail.com>
2018-12-27 07:47:16 +0900

    Google Chromeの起動を追加

--- startUp.bat
+++ startUp.bat
@@ -2,10 +2,10 @@
 
 REM 不要な処理はコメントアウトしてください。
 
-REM Outlook2013For32Bit
+REM Outlook2013For32Bitを開く
 REM start "" "c:\program files\microsoft office\office15\outlook.exe"
 
-REM Outlook2013For64Bit
+REM Outlook2013For64Bitを開く
 REM start "" "c:\program files (x86)\microsoft office\office15\outlook.exe"
 
 REM Outlook2016を開く
@@ -13,3 +13,6 @@ start "" "c:\program files\microsoft office\office16\outlook.exe"
 
 REM InternetExplorerでGithubを開く。
 start "" "C:\Program Files\Internet Explorer\iexplore.exe" "https://github.com/"
+
+REM Google ChromeでGithubを開く。
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "https://github.com/"
