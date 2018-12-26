7e49f2263310f582432af25e3cc0a173a0fc9b58
jumborin <y.f.architect@gmail.com>
2018-12-27 07:32:49 +0900

    フォルダパスを変更

--- backup.bat
+++ backup.bat
@@ -1,10 +1,10 @@
 @echo off
 
 REM バックアップ元フォルダ
-set SOURCE_DIR=C:\Users\jumborin\Desktop\test
+set SOURCE_DIR=.\test
 
 REM バックアップ先フォルダ
-set DIST_DIR=C:\Users\jumborin\Desktop\test2
+set DIST_DIR=.\test2
 
 REM コピー対象のファイルをログに出力する。
 xcopy %SOURCE_DIR% %DIST_DIR% /E /L /H /R /K /-Y /B > copyLog.log
