8218f9f61de2550050f128dddc3dec42dc9a11a8
jumborin <y.f.architect@gmail.com>
2018-12-28 19:34:05 +0900

    バックアップ処理バッチの名称を変更

--- backup.bat
+++ backup.bat
@@ -1,10 +1,10 @@
 @echo off
 
 REM バックアップ元フォルダ
-set SOURCE_DIR=.\test
+set SOURCE_DIR=.\source
 
 REM バックアップ先フォルダ
-set DIST_DIR=.\test2
+set DIST_DIR=.\dist
 
 REM コピー対象のファイルをログに出力する。
 xcopy %SOURCE_DIR% %DIST_DIR% /E /L /H /R /K /-Y /B > copyLog.log
