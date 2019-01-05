3fd596579bebb603848b531102f5a870c38d4369
Jumborin <jumborin@users.noreply.github.com>
2019-01-05 20:07:59 +0900

    イベントログIDをべた書きから変数に変更
    イベントログIDをべた書きから変数に変更

--- EventLogBatch/CreatePcOnOffEventList.bat
+++ EventLogBatch/CreatePcOnOffEventList.bat
@@ -6,41 +6,47 @@ REM Path設定
 REM -------------------------------------------------------
 
 REM tempファイル1
-set TEMP_FILE=.\temp1.tmp
+SET TEMP_FILE=.\temp1.tmp
 
 REM tempファイル2
-set TEMP_FILE2=.\temp2.tmp
+SET TEMP_FILE2=.\temp2.tmp
 
 REM 起動時刻と終了時刻のみを出力するテキストファイル
-set RESULT_FILE=.\result.csv
+SET RESULT_FILE=.\result.csv
+
+REM PC起動時のイベントログID
+SET START_LOG_ID=6005
+
+REM PCシャットダウン時のイベントログID
+SET FINISH_LOG_ID=6006
 
 REM -------------------------------------------------------
 REM 処理開始
 REM -------------------------------------------------------
 
 REM 先月の1日の日付を求める。
-set year=%date:~0,4%
-set month=%date:~5,2%
+SET year=%date:~0,4%
+SET month=%date:~5,2%
 if %month% equ 01 (
-  set month2=12
-  set /a "year2 = year - 1"
+  SET month2=12
+  SET /a "year2 = year - 1"
 ) else if %month% geq 11 (
-  set /a "month2 = month - 1"
-  set year2=%year%
+  SET /a "month2 = month - 1"
+  SET year2=%year%
 ) else (
-  set /a "month2_1 = month - 1"
-  set month2=0%month2_1%
-  set year2=%year%
+  SET /a "month2_1 = month - 1"
+  SET month2=0%month2_1%
+  SET year2=%year%
 )
 
 REM CSV形式でイベントログを吐き出す。
-wmic ntevent where "(logfile='system' and timewritten >= '%year2%%month2%01' and (eventcode='6005' or eventcode='6006'))" list /format:CSV | find "," > %TEMP_FILE%
+wmic ntevent where "(logfile='system' and timewritten >= '%year2%%month2%01' and (eventcode='%START_LOG_ID%' or eventcode='%FINISH_LOG_ID%'))" list /format:CSV | find "," > %TEMP_FILE%
 
 REM for内の変数を遅延展開する
 setlocal enabledelayedexpansion
   REM カンマが続いている部分をアンダースコアで置換する。
   for /f "delims=" %%i in (%TEMP_FILE%) do (
-    set tempStr=%%i
+    SET tempStr=%%i
     echo !tempStr:,,=,_,! >> %TEMP_FILE2%
   )
 
@@ -49,10 +55,10 @@ echo 分類,年,月,日,時,分,秒 > %RESULT_FILE%
 
 REM ファイルに結果を出力する。
 for /f "tokens=6,15 delims=," %%i in (%TEMP_FILE2%) do (
-  set tempTimeStr=%%j
-  if %%i equ 6005 (
+  SET tempTimeStr=%%j
+  if %%i equ %START_LOG_ID% (
     echo "起動時刻",!tempTimeStr:~0,4!,!tempTimeStr:~4,2!,!tempTimeStr:~6,2!,!tempTimeStr:~8,2!,!tempTimeStr:~10,2!,!tempTimeStr:~12,2!>> %RESULT_FILE%
-  ) else if %%i equ 6006 (
+  ) else if %%i equ %FINISH_LOG_ID% (
     echo "終了時刻",!tempTimeStr:~0,4!,!tempTimeStr:~4,2!,!tempTimeStr:~6,2!,!tempTimeStr:~8,2!,!tempTimeStr:~10,2!,!tempTimeStr:~12,2! >> %RESULT_FILE%
   ) else (
     echo "何もしない"
