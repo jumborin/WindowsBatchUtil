f7817ad915f6d6e7be30e7ac1ef3989e478c7a3a
jumborin <y.f.architect@gmail.com>
2019-01-11 22:31:57 +0900

    コメントを整理

--- CollectFileBatch/CollectOfficeFile.bat
+++ CollectFileBatch/CollectOfficeFile.bat
@@ -1,15 +1,24 @@
 @echo off
 
+REM ===============================================================
 REM officeDirを作成し、自ファイル以下のofficeファイルをコピーする。
-mkdir officeDir
-for /R %%f in (*.xls) do xcopy /D /I /Y "%%f" officeDir
-for /R %%f in (*.xlsx) do xcopy /D /I /Y "%%f" officeDir
-for /R %%f in (*.xlsm) do xcopy /D /I /Y "%%f" officeDir
-for /R %%f in (*.doc) do xcopy /D /I /Y "%%f" officeDir
-for /R %%f in (*.docx) do xcopy /D /I /Y "%%f" officeDir
-for /R %%f in (*.docm) do xcopy /D /I /Y "%%f" officeDir
-for /R %%f in (*.ppt) do xcopy /D /I /Y "%%f" officeDir
-for /R %%f in (*.pptx) do xcopy /D /I /Y "%%f" officeDir
-for /R %%f in (*.pptm) do xcopy /D /I /Y "%%f" officeDir
+REM ===============================================================
+
+REM フォルダ名を定義
+SET RESULT_DIR=officeDir
+
+REM フォルダを作成
+mkdir %RESULT_DIR%
+
+REM 各拡張子のファイルをコピーする。
+for /R %%f in (*.xls) do xcopy /D /I /Y "%%f" %RESULT_DIR%
+for /R %%f in (*.xlsx) do xcopy /D /I /Y "%%f" %RESULT_DIR%
+for /R %%f in (*.xlsm) do xcopy /D /I /Y "%%f" %RESULT_DIR%
+for /R %%f in (*.doc) do xcopy /D /I /Y "%%f" %RESULT_DIR%
+for /R %%f in (*.docx) do xcopy /D /I /Y "%%f" %RESULT_DIR%
+for /R %%f in (*.docm) do xcopy /D /I /Y "%%f" %RESULT_DIR%
+for /R %%f in (*.ppt) do xcopy /D /I /Y "%%f" %RESULT_DIR%
+for /R %%f in (*.pptx) do xcopy /D /I /Y "%%f" %RESULT_DIR%
+for /R %%f in (*.pptm) do xcopy /D /I /Y "%%f" %RESULT_DIR%
 
 
--- CollectFileBatch/CollectPdfFile.bat
+++ CollectFileBatch/CollectPdfFile.bat
@@ -1,5 +1,14 @@
 @echo off
 
+REM ===============================================================
 REM pdfDirを作成し、自ファイル以下のPDFファイルをコピーする。
-mkdir pdfDir
-for /R %%f in (*.pdf) do xcopy /D /I /Y "%%f" pdfDir
+REM ===============================================================
+
+REM フォルダ名を定義
+SET RESULT_DIR=pdfDir
+
+REM フォルダを作成
+mkdir %RESULT_DIR%
+
+REM PDFファイルをコピーする。
+for /R %%f in (*.pdf) do xcopy /D /I /Y "%%f" %RESULT_DIR%
