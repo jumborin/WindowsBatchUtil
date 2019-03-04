2f0c30acb336618460a74be5d5e3c6c16e40916c
jumborin <y.f.architect@gmail.com>
2019-03-04 22:14:18 +0900

    パスワード付きzipファイルを作成するバッチを新規作成

---
+++ CompressBatch/CompressBatch.bat
@@ -0,0 +1,22 @@
+@echo off
+
+REM ===============================================================
+REM 圧縮ファイルを本日日付8桁のパスワード付きzipファイルとして作成する。
+REM 前提条件として、同フォルダに7za.exeが格納されていること。
+REM ===============================================================
+
+REM 本日日付8桁(YYYYMMDD)
+SET YYYYMMDD=%DATE:/=%
+
+REM 圧縮後のファイル名
+SET COMPRESS_FILE_NAME=%YYYYMMDD%_compress.zip
+
+REM 圧縮対象のフォルダ
+SET COMPRESS_DIR=zip
+
+REM 解凍パスワード(本日日付8桁)
+SET PASS=%YYYYMMDD%
+
+REM 7zipを使って圧縮ファイルを作成する
+7za.exe a -p%PASS% %COMPRESS_FILE_NAME% %COMPRESS_DIR%
+
