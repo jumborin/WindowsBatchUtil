8827f72491130c4fdb7b3ab391ae4bc6adb28fe6
jumborin <y.f.architect@gmail.com>
2019-04-21 21:45:55 +0900

    JDK11用のJRE作成用バッチを新規作成

---
+++ CreateJREFromJDK/CreateJREFromJDF11.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM ==========================================================
+REM JDKから必要なモジュールのみ組み込んだJREを作成する。
+REM ==========================================================
+
+REM JDK11のPathを変数に設定する
+SET JDK_PATH="C:\Program Files\Java\jdk-11.0.2\jmods"
+
+REM JREを出力するフォルダ
+SET OUTPUT_DIR=jre-min
+
+
+REM JREを作成する
+jlink --compress=2 --module-path %JDK_PATH% --add-modules java.base --output %OUTPUT_DIR%
+
