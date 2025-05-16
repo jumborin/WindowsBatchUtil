acae39c8ee2dc8da7c62526bf5ec0c85270580b0
Jumborin <y.f.architect@gmail.com>
2025-05-16 19:55:51 +0900

    CreateReadmeFromDirBatchを追加

---
+++ CreateReadmeFromDirBatch/createReadmeFromDirBatch.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "self=%~nx0"
+set "output=README.txt"
+
+echo バッチファイル一覧と処理概要 > "%output%"
+echo =============================== >> "%output%"
+
+for /r %%f in (*.bat) do (
+    if /i not "%%~nxf"=="%self%" (
+        set "rel=%%f"
+        set "rel=!rel:%CD%\=!"
+
+        echo !rel! >> "%output%"
+
+        rem findstr で「処理概要」を含む行だけ抽出して書き込む
+        findstr /C:"処理概要" "%%f" >> "%output%"
+
+        echo. >> "%output%"
+    )
+)
+
+echo 完了しました。README.txt を確認してください。
+endlocal
+pause
